c8be0639fe7d635e5e642f48175b57f04d7ec67c
Andrew Erickson <aerickson@gmail.com>
2018-10-09 01:01:45 -0700

    more cleanup

--- src/main/java/com/hueemulator/emulator/CommandLine.java
+++ src/main/java/com/hueemulator/emulator/CommandLine.java
@@ -1,6 +1,5 @@
 package com.hueemulator.emulator;
 
-import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import java.util.concurrent.Callable;
@@ -8,24 +7,16 @@ import java.util.concurrent.Callable;
 @Command(name = "java -jar hue_emulator.jar", mixinStandardHelpOptions = true, version = "0.9")
 public class CommandLine implements Callable<Void>{
 
-    private static picocli.CommandLine cl;
-
-//    @Option(names = { "-v", "--verbose" }, description = "Verbose mode. Helpful for troubleshooting. " +
-//            "Multiple -v options increase the verbosity.")
-//    private boolean[] verbose = new boolean[0];
-
     @Parameters(arity = "0..1", paramLabel = "CONFIG_FILE", description = "Config file to use.")
     private static String configFile = null;
 
     @Override
-    public Void call() throws Exception {
-//        System.out.println("in call()");
+    public Void call() {
         new HueEmulator(configFile);
         return null;
     }
 
     public static void main(String[] args) {
-//        System.out.println("in main()");
         picocli.CommandLine.call(new CommandLine(), args);
     }
 
