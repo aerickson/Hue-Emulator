d555d5ac1a816c4e5ae3fa7a112ab28de5c3f3e9
Andrew Erickson <aerickson@gmail.com>
2018-10-09 01:00:18 -0700

    cleanup

--- src/main/java/com/hueemulator/emulator/CommandLine.java
+++ src/main/java/com/hueemulator/emulator/CommandLine.java
@@ -5,52 +5,28 @@ import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import java.util.concurrent.Callable;
 
-@Command(name = "example", mixinStandardHelpOptions = true, version = "Picocli example 3.0")
+@Command(name = "java -jar hue_emulator.jar", mixinStandardHelpOptions = true, version = "0.9")
 public class CommandLine implements Callable<Void>{
 
     private static picocli.CommandLine cl;
 
-    @Option(names = { "-v", "--verbose" }, description = "Verbose mode. Helpful for troubleshooting. " +
-            "Multiple -v options increase the verbosity.")
-    private boolean[] verbose = new boolean[0];
+//    @Option(names = { "-v", "--verbose" }, description = "Verbose mode. Helpful for troubleshooting. " +
+//            "Multiple -v options increase the verbosity.")
+//    private boolean[] verbose = new boolean[0];
 
     @Parameters(arity = "0..1", paramLabel = "CONFIG_FILE", description = "Config file to use.")
     private static String configFile = null;
 
     @Override
     public Void call() throws Exception {
-//        if (verbose.length > 0) {
-//            System.out.println(inputFiles.length + " files to process...");
-//        }
-//        if (verbose.length > 1) {
-//            for (File f : inputFiles) {
-//                System.out.println(f.getAbsolutePath());
-//            }
-//        }
-
-//        new CommandLine(args.length > 0 ? args[0] : null);
-//        if (CommandLine.isVersionHelpRequested() || CommandLine.isUsageHelpRequested()){
-//            // do nothing
-//        }
-//        else {
-//            new CommandLine(configFile);
-//        }
-        System.out.println("in call()");
+//        System.out.println("in call()");
         new HueEmulator(configFile);
         return null;
     }
 
     public static void main(String[] args) {
-        System.out.println("in main()");
-
-        // TODO: how to store CL below into cl?
+//        System.out.println("in main()");
         picocli.CommandLine.call(new CommandLine(), args);
-//        new CommandLine(args.length > 0 ? args[0] : null);
     }
-
-//    public static void main(String args[]) {
-//        new CommandLine(args.length > 0 ? args[0] : null);
-//    }
-    
 
 }
